518eabdba7c3385b680315b373fc39f8bbf8c21b
Ray <eebolue@gmail.com>
2018-08-31 13:53:16 -0400

    Refactor ViewModel & Input Binding Function
    - rename RYFloatingInput.HintVisibility
    - rename RYFloatingInputViolation to RYFloatingInput.InputViolation

--- RYFloatingInput/Classes/RYFloatingInputSetting.swift
+++ RYFloatingInput/Classes/RYFloatingInputSetting.swift
@@ -8,47 +8,8 @@
 
 import UIKit
 
-public class RYFloatingInputSetting {
+public extension RYFloatingInputSetting {
 
-    internal var backgroundColor: UIColor
-    internal var textColor: UIColor
-    internal var placeholderColor: UIColor
-    internal var dividerColor: UIColor
-    internal var cursorColor: UIColor
-    internal var accentColor: UIColor
-    internal var warningColor: UIColor
-    
-    internal var iconImage: UIImage?
-    internal var dividerHeight: CGFloat
-    internal var placeholder: String?
-    internal var isSecure: Bool = false
-    
-    internal var maxLength: Int?
-    internal var maxLengthViolation: RYFloatingInputViolation?
-    internal var inputType: RYFloatingInput.InputType?
-    internal var inputTypeViolation: RYFloatingInputViolation?
-    
-    private init(builder: Builder) {
-        
-        self.backgroundColor = builder._backgroundColor ?? builder._theme.background
-        self.textColor = builder._textColor ?? builder._theme.text
-        self.placeholderColor = builder._placeholderColor ?? builder._theme.placeholder
-        self.dividerColor = builder._dividerColor ?? builder._theme.divider
-        self.cursorColor = builder._cursorColor ?? builder._theme.cursor
-        self.accentColor = builder._accentColor ?? builder._theme.accent
-        self.warningColor = builder._warningColor ?? builder._theme.warning
-
-        self.iconImage = builder._iconImage
-        self.dividerHeight = builder._dividerWeight.rawValue
-        self.placeholder = builder._placeholder
-        self.isSecure = builder._secure
-
-        self.maxLength = builder._maxLength
-        self.maxLengthViolation = builder._maxLengthViolation
-        self.inputType = builder._inputType
-        self.inputTypeViolation = builder._inputTypeViolation
-    }
-    
     public class Builder {
 
         fileprivate var _theme: RYFloatingInput.Theme = .standard
@@ -64,12 +25,12 @@ public class RYFloatingInputSetting {
         fileprivate var _dividerWeight: RYFloatingInput.DividerWeight = .regular
         fileprivate var _placeholder: String?
         fileprivate var _secure: Bool = false
-        
+
         fileprivate var _maxLength: Int?
-        fileprivate var _maxLengthViolation: RYFloatingInputViolation?
+        fileprivate var _maxLengthViolation: RYFloatingInput.InputViolation?
         fileprivate var _inputType: RYFloatingInput.InputType?
-        fileprivate var _inputTypeViolation: RYFloatingInputViolation?
-        
+        fileprivate var _inputTypeViolation: RYFloatingInput.InputViolation?
+
         public static func instance() -> Builder {
             return Builder()
         }
@@ -93,27 +54,27 @@ public class RYFloatingInputSetting {
             _placeholderColor = color
             return self
         }
-        
+
         public func dividerColor(_ color: UIColor) -> Builder {
             _dividerColor = color
             return self
         }
-        
+
         public func cursorColor(_ color: UIColor) -> Builder {
             _cursorColor = color
             return self
         }
-        
+
         public func accentColor(_ color: UIColor) -> Builder {
             _accentColor = color
             return self
         }
-        
+
         public func warningColor(_ color: UIColor) -> Builder {
             _warningColor = color
             return self
         }
-        
+
         public func iconImage(_ image: UIImage) -> Builder {
             _iconImage = image
             return self
@@ -128,19 +89,19 @@ public class RYFloatingInputSetting {
             _placeholder = placeholder
             return self
         }
-        
+
         public func secure(_ isSecure: Bool) -> Builder {
             _secure = isSecure
             return self
         }
-        
-        public func maxLength(_ length: Int, onViolated violation: RYFloatingInputViolation) -> Builder {
+
+        public func maxLength(_ length: Int, onViolated violation: RYFloatingInput.InputViolation) -> Builder {
             _maxLength = length
             _maxLengthViolation = violation
             return self
         }
-        
-        public func inputType(_ type: RYFloatingInput.InputType, onViolated violation: RYFloatingInputViolation) -> Builder {
+
+        public func inputType(_ type: RYFloatingInput.InputType, onViolated violation: RYFloatingInput.InputViolation) -> Builder {
             _inputType = type
             _inputTypeViolation = violation
             return self
@@ -150,5 +111,49 @@ public class RYFloatingInputSetting {
             return RYFloatingInputSetting(builder: self)
         }
     }
+}
+
+public class RYFloatingInputSetting {
+
+    internal let backgroundColor: UIColor
+    internal let textColor: UIColor
+    internal let placeholderColor: UIColor
+    internal let dividerColor: UIColor
+    internal let cursorColor: UIColor
+    internal let accentColor: UIColor
+    internal let warningColor: UIColor
+
+    internal let iconImage: UIImage?
+    internal let dividerHeight: CGFloat
+    internal let placeholder: String?
+    internal let isSecure: Bool?
+
+    internal let maxLength: Int?
+    internal let maxLengthViolation: RYFloatingInput.InputViolation?
+    internal let inputType: RYFloatingInput.InputType?
+    internal let inputTypeViolation: RYFloatingInput.InputViolation?
+
+    private init(builder: Builder) {
+
+        self.backgroundColor = builder._backgroundColor ?? builder._theme.background
+        self.textColor = builder._textColor ?? builder._theme.text
+        self.placeholderColor = builder._placeholderColor ?? builder._theme.placeholder
+        self.dividerColor = builder._dividerColor ?? builder._theme.divider
+        self.cursorColor = builder._cursorColor ?? builder._theme.cursor
+        self.accentColor = builder._accentColor ?? builder._theme.accent
+        self.warningColor = builder._warningColor ?? builder._theme.warning
+
+        self.iconImage = builder._iconImage
+        self.dividerHeight = builder._dividerWeight.rawValue
+        self.placeholder = builder._placeholder
+        self.isSecure = builder._secure
+
+        self.maxLength = builder._maxLength
+        self.maxLengthViolation = builder._maxLengthViolation
+        self.inputType = builder._inputType
+        self.inputTypeViolation = builder._inputTypeViolation
+    }
+
+
 }
 
